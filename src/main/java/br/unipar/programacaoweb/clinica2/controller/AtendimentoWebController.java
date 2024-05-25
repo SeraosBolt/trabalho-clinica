f7a1384d4aee3904fc8ad8d40002def85a9b511d
MayconFelipeBianchi <114965402+MayconFelipeBianchi@users.noreply.github.com>
2024-05-24 23:02:14 -0300

    Finalização

--- src/main/java/br/unipar/programacaoweb/clinica2/controller/AtendimentoWebController.java
+++ src/main/java/br/unipar/programacaoweb/clinica2/controller/AtendimentoWebController.java
@@ -1,11 +1,7 @@
 package br.unipar.programacaoweb.clinica2.controller;
 
-import br.unipar.programacaoweb.clinica2.model.Atendimento;
-import br.unipar.programacaoweb.clinica2.model.Medico;
-import br.unipar.programacaoweb.clinica2.model.Usuario;
-import br.unipar.programacaoweb.clinica2.service.AtendimentoService;
-import br.unipar.programacaoweb.clinica2.service.MedicoService;
-import br.unipar.programacaoweb.clinica2.service.UsuarioService;
+import br.unipar.programacaoweb.clinica2.model.*;
+import br.unipar.programacaoweb.clinica2.service.*;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,25 +24,28 @@ public class AtendimentoWebController {
 
     private  AtendimentoService atendimentoService;
     private MedicoService medicoService;
-
-    public AtendimentoWebController(AtendimentoService atendimentoService, MedicoService medicoService) {
+    private PacienteService pacienteService;
+    public AtendimentoWebController(AtendimentoService atendimentoService, MedicoService medicoService, PacienteService pacienteService) {
         this.atendimentoService = atendimentoService;
         this.medicoService =  medicoService;
+        this.pacienteService = pacienteService;
     }
 
     @GetMapping(path = "/atendimento")
     public String getAllAtendimento(Model model) {
         List<Atendimento> atendimento = atendimentoService.getAll();
         List<Medico> medicos = medicoService.getAll();
+        List<Paciente> pacientes = pacienteService.getAll();
         model.addAttribute("atendimento", atendimento);
         model.addAttribute("medicos", medicos);
+        model.addAttribute("pacientes", pacientes);
         return "atendimento";
     }
 
     @PostMapping(path = "/atendimento/save")
     public String saveAtendimento(Atendimento atendimento) {
+        System.out.println(atendimento);
         atendimentoService.save(atendimento);
         return "redirect:/atendimento";
     }
-
 }
--- src/main/java/br/unipar/programacaoweb/clinica2/model/Atendimento.java
+++ src/main/java/br/unipar/programacaoweb/clinica2/model/Atendimento.java
@@ -24,9 +24,6 @@ public class Atendimento implements Serializable {
     @ManyToOne
     private Medico medico;
 
-
-
     @ManyToOne
     private Paciente paciente;
-
 }
